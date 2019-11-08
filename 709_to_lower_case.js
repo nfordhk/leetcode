2c82dbbf6d3b32cfa5a3663b066c6dd9129aa75f
Nick Ford <fordn@fordn-W04.vmware.com>
2019-11-07 16:23:59 -0800

    work for leetcode problem toLower

--- 709_to_lower_case.js
+++ 709_to_lower_case.js
@@ -1,13 +1,28 @@
 /*
- * @param {string} str
- * @return {string}
- */
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+//Most languages support lowercase conversion for a string data type. However, that is certainly not the purpose of the problem. 
+Think about how the implementation of the lowercase function call can be done easily.
+*/
 
 //Solution 1, using toLowerCase built in method
-var toLowerCase = function(str) {
+/*var toLowerCase = function(str) {
     return str.toLowerCase()
-};
+};*/
 
-//Solution 2, using ASCII
+//Solution 2, using ASCII (incompleted solution)
+function toLowerCase () {
+    let str = "Hello"
+    const regex = /[A-Z]/g;
 
+    if (str.match(regex)) {
+        let regexString = str.match(regex)
+        let asciiCode = str.charCodeAt(0);
+
+        //asciiCode = asciiCode + 32
+        asciiCode += 32
+        return asciiCode
+    }
+}
+
+console.log (toLowerCase())
 
